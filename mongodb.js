6f8cc35124fb6958ca3744220ac1146d06ce5b1a
SheaMeyers <shea.meyers@gmail.com>
2019-09-19 20:20:11 +0200

    Allow adding multiple items at once

--- mongodb.js
+++ mongodb.js
@@ -13,8 +13,60 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName)
 
-    db.collection('users').insertOne({
-        name: 'Shea',
-        age: 28
+    // db.collection('users').insertOne({
+    //     name: 'Shea',
+    //     age: 28
+    // }, (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert user')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Jen',
+    //         age: 28
+    //     }, {
+    //         name: 'Gunther',
+    //         age: 27
+    //     }
+    // ], (error, result) => {
+    //     if (error) {
+    //         return console.log("Unable to insert documents")
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    //
+    // Goal: Insert 3 tasks into a new tasks collection
+    //
+    // 1. Use insertMany to insert three documents
+    //      - description (string), completed (boolean)
+    // 2. Setup the callback to handle error or print ops
+    // 3. Run the script
+    // 4. Refresh the database in Robo 3T and view data in tasks collection
+
+    db.collection('tasks').insertMany([
+        {
+            description: "Wake up",
+            completed: true
+        }, 
+        {
+            description: "Go to work",
+            completed: true
+        }, 
+        {
+            description: "Go home",
+            completed: false
+        }, 
+    ], (error, result) => {
+        if (error) {
+            return console.log("Unable to insert documents")
+        }
+
+        console.log(result.ops)
     })
 })
