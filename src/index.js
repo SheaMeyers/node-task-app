519806225f4981f8a1a9db6426b50186dfd363a4
SheaMeyers <shea.meyers@gmail.com>
2019-10-09 19:24:44 +0200

    Add endpoints for gettings users and tasks

--- src/index.js
+++ src/index.js
@@ -18,6 +18,28 @@ app.post('/users', (req, res) => {
     })
 })
 
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch((error) => {
+        res.status(500).send(error)
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+
+    User.findById(_id).then((user) => {
+        if (!user) {
+            res.status(404).send()
+        } else {
+            res.send(user)
+        }
+    }).catch((error) => {
+        res.status(500).send(error)
+    })
+})
+
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body)
 
@@ -25,6 +47,28 @@ app.post('/tasks', (req, res) => {
         res.send(task)
     }).catch((error) => {
         res.status(400).send(error)
+    })
+})
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((error) => {
+        res.status(500).send(error)
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id
+
+    Task.findById(_id).then((task) => {
+        if (!task) {
+            res.status(404).send()
+        } else {
+            res.send(task)
+        }
+    }).catch((error) => {
+        res.status(500).send(error)
     })
 })
 
