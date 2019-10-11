4f694fbb0df146d2a63576554a21e052a15b4555
SheaMeyers <shea.meyers@gmail.com>
2019-10-11 20:37:49 +0200

    Add endpoints for updating users and tasks

--- src/index.js
+++ src/index.js
@@ -39,7 +39,28 @@ app.get('/users/:id', async (req, res) => {
         } 
         res.send(user)
     } catch (e) {
-        res.status(500).send(error)
+        res.status(500).send(e)
+    }
+})
+
+app.patch('/users/:id', async (req, res) => {
+    
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        res.status(400).send({ error: 'Invalid updates!'})
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        if (!user) {
+            res.status(404).send()
+        } 
+        res.send(user)
+    } catch (e) {
+        res.status(400).send(e)
     }
 })
 
@@ -50,7 +71,7 @@ app.post('/tasks', async (req, res) => {
         await task.save()
         res.send(task)
     } catch (e) {
-        res.status(400).send(error)
+        res.status(400).send(e)
     }
 })
 
@@ -75,7 +96,29 @@ app.get('/tasks/:id', async (req, res) => {
             res.send(task)
         }
     } catch (e) {
-        res.status(500).send(error)
+        res.status(500).send(e)
+    }
+})
+
+app.patch('/tasks/:id', async (req, res) => {
+    
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        res.status(400).send({ error: 'Invalid updates!'})
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        if (!task) {
+            res.status(404).send()
+        } 
+        res.send(task)
+    } catch (e) {
+        console.log(e)
+        res.status(400).send(e)
     }
 })
 
