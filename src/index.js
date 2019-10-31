15adf54f711856bc1cdd2b6e7fca1663897785da
SheaMeyers <shea.meyers@gmail.com>
2019-10-31 18:57:04 +0100

    Add express middleware

--- src/index.js
+++ src/index.js
@@ -7,9 +7,20 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use((req, res, next) => {
+    if (req.method === 'GET') {
+        res.send('Get requests are disabled')
+    } else {
+        next()
+    }
+})
+
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
+
+
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
