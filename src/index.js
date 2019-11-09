5331adf14a788ee77efb268b65c225ab726a2620
SheaMeyers <shea.meyers@gmail.com>
2019-11-09 19:48:24 +0100

    Remove testing code

--- src/index.js
+++ src/index.js
@@ -15,18 +15,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
-
-const Task = require('./models/task')
-const User = require('./models/user')
-
-const main = async () => {
-    // const task = await Task.findById('5dc59bbfe4e50e33e2a60ba8')
-    // await task.populate('owner').execPopulate()
-    // console.log(task.owner);
-
-    const user = await User.findById('5dc598cf2018f033828d5f3c')
-    await user.populate('tasks').execPopulate()
-    console.log(user.tasks)
-}
-
-main()
