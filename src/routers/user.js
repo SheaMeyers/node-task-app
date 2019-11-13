82165b9a021e3c451dde0a5c752a54793bad0813
SheaMeyers <shea.meyers@gmail.com>
2019-11-13 20:18:15 +0100

    Add endpoint for uploading image/avatar

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const multer = require('multer')
 const router = new express.Router()
 const auth = require('../middleware/auth')
 const User = require('../models/user')
@@ -51,8 +52,6 @@ router.get('/users/me', auth, async (req, res) => {
     res.send(req.user)
 })
 
-
-
 router.patch('/users/me', auth, async (req, res) => {
     
     const updates = Object.keys(req.body)
@@ -79,6 +78,14 @@ router.delete('/users/me', auth, async (req, res) => {
     } catch (e) {
         res.status(500).send(e)
     }
+})
+
+const upload = multer({
+    dest: 'avatars'
+})
+
+router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+    res.send()
 })
 
 module.exports = router
