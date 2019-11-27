a31996601b0b0c96e034771849fe45ccd3eb6874
SheaMeyers <shea.meyers@gmail.com>
2019-11-27 07:40:02 +0100

    Add unit test for logging a user in

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,5 +1,17 @@
 const request = require('supertest')
 const app = require('../src/app')
+const User = require('../src/models/user')
+
+const userOne = {
+    name: 'Mike',
+    email: 'mike@example.com',
+    password: '56what!!'
+}
+
+beforeEach(async () => {
+    await User.deleteMany()
+    await new User(userOne).save()
+})
 
 test('Should sign up a new user', async () => {
     await request(app).post('/users').send({
@@ -8,3 +20,18 @@ test('Should sign up a new user', async () => {
         password: 'MyPass777!'
     }).expect(201)
 })
+
+test('should log in existing user', async () => {
+    await request(app).post('/users/login').send({
+        email: userOne.email,
+        password: userOne.password
+    }).expect(200)
+})
+
+test('should not log in non existant user', async () => {
+    await request(app).post('/users/login').send({
+        email: 'random@email.com',
+        password: 'randomPassword'
+    }).expect(400)
+})
+
