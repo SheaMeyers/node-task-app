6cd39429c17a3b14f8a6de49fbb971e663dab3b2
SheaMeyers <shea.meyers@gmail.com>
2019-12-01 12:36:00 +0100

    Use authentication when testing

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,18 +4,18 @@ const User = require('../models/user')
 const auth = async (req, res, next) => {
     try {
         const token = req.header('Authorization').replace('Bearer ', '')
-        const decoded = jwt.verify(token, 'thisismynewcourse')
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
         const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
 
         if (!user) {
             throw new Error()
         }
 
-        req.user = user
         req.token = token
+        req.user = user
         next()
     } catch (e) {
-        res.status(401).send({ error: 'Please authenticate' })
+        res.status(401).send({ error: 'Please authenticate.' })
     }
 }
 
