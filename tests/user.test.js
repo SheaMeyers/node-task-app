a058dcc612947ac8931fd4ebc4f77fe201c1a1f5
SheaMeyers <shea.meyers@gmail.com>
2019-12-04 08:08:25 +0100

    Add additional assertions in unit tests

--- tests/user.test.js
+++ tests/user.test.js
@@ -21,18 +21,40 @@ beforeEach(async () => {
 })
 
 test('Should sign up a new user', async () => {
-    await request(app).post('/users').send({
+    const response = await request(app).post('/users').send({
         name: 'Shea',
         email: 'shea@example.com',
         password: 'MyPass777!'
     }).expect(201)
+
+    // Assert that the user exists in the database
+    const user = await User.findById(response.body.user._id)
+    expect(user).not.toBeNull()
+
+    // Assertions about the user that was created
+    expect(response.body).toMatchObject({
+        user: {
+            name: 'Shea',
+            email: 'shea@example.com'
+        },
+        token: user.tokens[0].token
+    })
+    expect(user.password).not.toBe('MyPass777!')
 })
 
 test('should log in existing user', async () => {
-    await request(app).post('/users/login').send({
+    const response = await request(app).post('/users/login').send({
         email: userOne.email,
         password: userOne.password
     }).expect(200)
+
+
+    // Assert that the user exists in the database
+    const user = await User.findById(response.body.user._id)
+    expect(user).not.toBeNull()
+
+    // Assert that the token is saved in the DB
+    expect(response.body.token).toBe(user.tokens[1].token)
 })
 
 test('should not log in non existant user', async () => {
@@ -58,15 +80,19 @@ test('Should not get profile for unauthenticated user', async () => {
 })
 
 test('Should delete account for user', async () => {
-    await request(app)
+    const response = await request(app)
         .delete('/users/me')
         .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
         .send()
         .expect(200)
+
+    // Assert that the user exists in the database
+    const user = await User.findById(userOneId)
+    expect(user).toBeNull()
 })
 
 test('Should not delete account for unauthenticated user', async () => {
-    await request(app)
+    const response = await request(app)
         .delete('/users/me')
         .send()
         .expect(401)
